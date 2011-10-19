4416fce3c334fdc721a2896a4776c15b8a37f746
Jim Wallace <jfwallac@gmail.com>
2011-10-18 21:10:50 -0400

    Changed the background colors to white, initial style thoughts

--- src/com/mapitprices/WheresTheCheapBeer/Constants.java
+++ src/com/mapitprices/WheresTheCheapBeer/Constants.java
@@ -9,7 +9,6 @@ package com.mapitprices.WheresTheCheapBeer;
  */
 public class Constants {
     public static final boolean DEBUGMODE = false;
-    public static final boolean FoursquareEnabled = true;
     public static final int MIN_DISTANCE = 100; // in meters
     public static final int MIN_TIME = 120000; // 2 minutes in ms
     public static final int APIVERSION = 2;
